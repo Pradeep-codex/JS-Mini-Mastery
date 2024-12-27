7a41de070842fef027753b9578024d8c54ce7e07
Pradeep-codex <pradeep2003c@gmail.com>
2024-12-27 21:52:51 +0530

    Add Project: Rotating Image Gallery

---
+++ Rotating-image-gallery/script.js
@@ -0,0 +1,21 @@
+const imgCon = document.querySelector(".img-con");
+const prevEle = document.getElementById("prev")
+const nextEle = document.getElementById("next")
+let x = 0;
+prevEle.addEventListener("click",()=>{
+    x = x + 45;
+    updateGallery()
+})
+
+function updateGallery(){
+    imgCon.style.transform = `perspective(1000px) rotateY(${x}deg)` 
+}
+
+nextEle.addEventListener("click",()=>{
+    x = x - 45;
+    updateGallerys()
+})
+
+function updateGallerys(){
+    imgCon.style.transform = `perspective(1000px) rotateY(${x}deg)` 
+}
